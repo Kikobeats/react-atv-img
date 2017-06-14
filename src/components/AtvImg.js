b4f5cb4b51d2220c73ce26d8d89249b54aa3c6c0
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-14 18:41:19 +0200

    Improve build process

--- src/components/AtvImg.js
+++ src/components/AtvImg.js
@@ -1,45 +1,45 @@
-import React, { Component, PropTypes } from 'react';
+import React, { createClass, PropTypes } from 'react';
 
 import styles from '../styles';
 
-export default class AtvImg extends Component {
-  static propTypes = {
-    layers: PropTypes.arrayOf(PropTypes.string),
+export default createClass({
+  propTypes: {
+    layers: PropTypes.arrayOf(PropTypes.string).isRequired,
     isStatic: PropTypes.bool,
     staticFallback: PropTypes.string,
     className: PropTypes.string,
     style: PropTypes.object,
-    children: PropTypes.node,
-  };
+  },
 
-  state = {
-    rootElemWidth: 0,
-    rootElemHeight: 0,
-    isOnHover: false,
-    container: {},
-    shine: {},
-    layers: [],
-  };
+  getInitialState() {
+    return {
+      rootElemWidth: 0,
+      rootElemHeight: 0,
+      isOnHover: false,
+      container: {},
+      shine: {},
+      layers: [],
+    }
+  },
 
   componentDidMount() {
     if (!this.props.isStatic) {
       this.setState({ // eslint-disable-line react/no-did-mount-set-state
         // this is a legit use case. we must trigger a re-render. don't worry.
-        rootElemWidth: this.refs.root.clientWidth || this.refs.root.offsetWidth || this.refs.root.scrollWidth,
-        rootElemHeight: this.refs.root.clientHeight || this.refs.root.offsetHeight || this.refs.root.scrollHeight,
+        rootElemWidth: this.root.clientWidth || this.root.offsetWidth || this.root.scrollWidth,
+        rootElemHeight: this.root.clientHeight || this.root.offsetHeight || this.root.scrollHeight,
       });
     }
-  }
+  },
 
-  handleMove = ({ pageX, pageY }) => {
-    const allLayers = this.allLayers();
-    const layerCount = allLayers ? this.allLayers.length : 0; // the number of layers
+  handleMove ({ pageX, pageY }) {
+    const layerCount = this.props.layers.length; // the number of layers
 
     const { rootElemWidth, rootElemHeight } = this.state;
 
     const bodyScrollTop = document.body.scrollTop || document.getElementsByTagName('html')[0].scrollTop;
     const bodyScrollLeft = document.body.scrollLeft;
-    const offsets = this.refs.root.getBoundingClientRect();
+    const offsets = this.root.getBoundingClientRect();
     const wMultiple = 320 / rootElemWidth;
     const offsetX = 0.52 - (pageX - offsets.left - bodyScrollLeft) / rootElemWidth; // cursor position X
     const offsetY = 0.52 - (pageY - offsets.top - bodyScrollTop) / rootElemHeight; // cursor position Y
@@ -61,83 +61,59 @@ export default class AtvImg extends Component {
         background: `linear-gradient(${angle}deg, rgba(255, 255, 255, ${(pageY - offsets.top - bodyScrollTop) / rootElemHeight * 0.4}) 0%, rgba(255, 255, 255, 0) 80%)`,
         transform: `translateX(${(offsetX * layerCount) - 0.1}px) translateY(${(offsetY * layerCount) - 0.1}px)`,
       },
-      layers: allLayers.map((_, idx) => ({
+      layers: this.props.layers.map((_, idx) => ({
         transform: `translateX(${(offsetX * (layerCount - idx)) * ((idx * 2.5) / wMultiple)}px) translateY(${offsetY * layerCount * ((idx * 2.5) / wMultiple)}px)`,
       })),
     });
-  }
+  },
 
-  handleTouchMove = (evt) => {
+  handleTouchMove (evt) {
     evt.preventDefault();
     const { pageX, pageY } = evt.touches[0];
     this.handleMove({ pageX, pageY });
-  }
+  },
 
-  handleEnter = () => {
+  handleEnter () {
     this.setState({ isOnHover: true });
-  }
+  },
 
-  handleLeave = () => {
+  handleLeave () {
     this.setState({
       isOnHover: false,
       container: {},
       shine: {},
       layers: [],
     });
-  }
+  },
 
-  allLayers = () => {
-    let layers = this.props.layers || [];
+  renderShadow () {
+    return (
+      <div style={{ ...styles.shadow, ...(this.state.isOnHover ? styles.shadowOnHover : {}) }}/>
+    )
+  },
 
-    if (typeof this.props.children === 'object') {
-      layers = this.props.children.constructor === Array ?
-        layers.concat(this.props.children) : layers.concat([this.props.children]);
-    }
-
-    return layers;
-  }
-
-  renderShadow = () => (
-    <div style={{ ...styles.shadow, ...(this.state.isOnHover ? styles.shadowOnHover : {}) }}/>
-  );
-
-  renderLayers = () => {
-    const allLayers = this.allLayers();
-
+  renderLayers () {
     return (
       <div style={styles.layers}>
-        {allLayers && allLayers.map((layer, idx) => {
-          if (typeof layer === 'string') {
-            return (
-              <div
-                style={{
-                  backgroundImage: `url(${layer})`,
-                  ...styles.renderedLayer,
-                  ...(this.state.layers[idx] ? this.state.layers[idx] : {}),
-                }}
-                key={idx}
-              />
-            );
-          }
-
-          return React.Children.map(layer,
-            child => React.cloneElement(child, {
-              style: {
-                ...child.props.style,
-                ...styles.root,
-                ...(this.props.style ? this.props.style : {}),
-                ...this.state.layers[idx],
-              },
-              className: `${child.props.className} ${this.props.className || ''}`,
-            }));
-        })}
+        {this.props.layers && this.props.layers.map((imgSrc, idx) => (
+          <div
+            style={{
+              backgroundImage: `url(${imgSrc})`,
+              ...styles.renderedLayer,
+              ...(this.state.layers[idx] ? this.state.layers[idx] : {}),
+            }}
+            key={idx}
+          />
+        ))}
       </div>
-    );
-  }
+    )
+  },
 
-  renderShine = () => (
-    <div style={{ ...styles.shine, ...this.state.shine }}/>
-  );
+  renderShine () {
+    return (
+      <div style={{ ...styles.shine, ...this.state.shine }}/>
+    )
+  },
 
   render() {
     if (this.props.isStatic) {
@@ -150,7 +126,6 @@ export default class AtvImg extends Component {
           className={this.props.className || ''}
         >
           <img style={styles.staticFallback} src={this.props.staticFallback} />
-          {this.props.children}
         </div>
       );
     }
@@ -169,7 +144,7 @@ export default class AtvImg extends Component {
         onTouchStart={this.handleEnter}
         onTouchEnd={this.handleLeave}
         className={this.props.className || ''}
-        ref="root"
+        ref={node => (this.root = node)}
       >
         <div style={{ ...styles.container, ...this.state.container }}>
           {this.renderShadow()}
@@ -179,4 +154,4 @@ export default class AtvImg extends Component {
       </div>
     );
   }
-}
+})
